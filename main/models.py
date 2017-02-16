7773311a1cdc545a7cb9a8d65aa777d1926c913f
cfc603 <wtrevor162@gmail.com>
2017-02-16 16:14:04 -0500

    create Blog & Category models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
 
 
+class CreatedUpdated(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Blog(CreatedUpdated):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    live = models.BooleanField(default=False)
+    category = models.ManyToManyField("main.Category")
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @property
+    def slug(self):
+        return slugify(self.title)
+
+
+class Category(CreatedUpdated):
+    title = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return "%s" % self.title
+
+    @property
+    def slug(self):
+        return slugify(self.title)
--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
+from model_mommy import mommy
 
+
+class BlogTest(TestCase):
+
+    def test_unicode(self):
+        blog = mommy.make("main.Blog")
+        self.assertEqual(blog.__unicode__(), blog.title)
+
+    def test_slug(self):
+        blog = mommy.make("main.Blog", title="Here Is A Test title")
+        self.assertEqual(blog.slug, "here-is-a-test-title")
+
+
+class CategoryTest(TestCase):
+
+    def test_unicode(self):
+        category = mommy.make("main.Category")
+        self.assertEqual(category.__unicode__(), category.title)
+
+    def test_slug(self):
+        category  = mommy.make(
+            "main.Category", title="Test Category Title"
+        )
+        self.assertEqual(category.slug, "test-category-title")
