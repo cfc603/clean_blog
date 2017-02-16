846d20e947d083d88dbb6edd0af5bf9906bd600e
cfc603 <wtrevor162@gmail.com>
2017-02-16 15:26:59 -0500

    Update ContactView to subclass FormView added form_class, success_url

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from main.forms import ContactForm
+
 
 class AboutViewTest(TestCase):
 
@@ -21,6 +23,18 @@ class ContactViewTest(TestCase):
     def test_renders_correct_template(self):
         response = self.client.get(reverse("main:contact"))
         self.assertTemplateUsed(response, "main/contact.html")
+
+    def test_uses_contact_form_as_form_class(self):
+        response = self.client.get(reverse("main:contact"))
+        self.assertIsInstance(response.context['form'], ContactForm)
+
+    def test_uses_correct_success_url(self):
+        response = self.client.post(reverse("main:contact"), {
+                "name": "test name",
+                "email": "test@test.com",
+                "message": "test message",
+            })
+        self.assertRedirects(response, reverse("main:contact_form_success"))
 
 
 class HomeViewTest(TestCase):
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+
+from .forms import ContactForm
 
 
 class AboutView(TemplateView):
@@ -11,6 +15,13 @@ class ContactFormSuccessView(TemplateView):
 
 class ContactView(FormView):
     template_name = "main/contact.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("main:contact_form_success")
+
+    def form_valid(self, form):
+        form.send_email()
+        form.send_confirmation()
+        return super(ContactView, self).form_valid(form)
 
 
 class HomeView(TemplateView):
