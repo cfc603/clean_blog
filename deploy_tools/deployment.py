21845729c66abf1efc6e70c17b557742c8627253
cfc603 <wtrevor162@gmail.com>
2017-02-23 14:52:02 -0500

    fix typo

--- deploy_tools/deployment.py
+++ deploy_tools/deployment.py
@@ -49,7 +49,7 @@ class Deployment(object):
     def run_tests(self):
         with cd(self.server.source_directory):
             with prefix('workon {}'.format(self.server.website)):
-                for app in settings.APPS_TO_TEST:
+                for app in dj_settings.APPS_TO_TEST:
                     run('python manage.py test {}'.format(app))
 
     def update_secrets(self):
