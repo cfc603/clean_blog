0dda3f344e9bde24bf7b3ca2fe7bd85d949344a9
cfc603 <wtrevor162@gmail.com>
2017-03-02 14:29:18 -0500

    fix error with path to ssl_domain_file

--- deploy_tools/certbot.py
+++ deploy_tools/certbot.py
@@ -49,14 +49,16 @@ class Certbot(object):
         file = "ssl-{}.conf".format(self.url)
 
         if not exists(file):
+            file_path = "{}/{}".format(
+                self.nginx_snippet_directory, file
+            )
             sudo(
-                "cp {}/ssl-domain.template.conf {}/{}".format(
+                "cp {}/ssl-domain.template.conf {}".format(
                     self.template_directory,
-                    self.nginx_snippet_directory,
-                    file
+                    file_path,
                 )
             )
-            sed(file, "DOMAIN", self.url, use_sudo=True)
+            sed(file_path, "DOMAIN", self.url, use_sudo=True)
 
         return file
 
